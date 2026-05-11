5406f0d1d398e96a21a641a9428c40145d30442f
MarizPOG <tpmariz04@gmail.com>
2026-05-11 20:11:06 +0200

    -fixed rendering of dynamic sublevels when using shaders, changed from rendering at WorldRenderEvents.LAST to HudRenderCallback.EVENT, also made it only renderable in AtlasScreen - changed version of the mod to 1.2.1, Surveyor to 1.2.4

--- src/main/java/me/mariz/antique_transport/client/AntiqueTransportClient.java
+++ src/main/java/me/mariz/antique_transport/client/AntiqueTransportClient.java
@@ -4,6 +4,7 @@ import dev.ryanhcode.sable.companion.SableCompanion;
 import dev.ryanhcode.sable.companion.SubLevelAccess;
 import dev.ryanhcode.sable.sublevel.ClientSubLevel;
 import folk.sisby.antique_atlas.gui.AtlasRenderer;
+import folk.sisby.antique_atlas.gui.AtlasScreen;
 import me.mariz.antique_transport.client.compat.ModCompat;
 import me.mariz.antique_transport.client.compat.create.CreateClientCompat;
 import me.mariz.antique_transport.client.compat.sable.SableClientCompat;
@@ -12,6 +13,7 @@ import me.mariz.antique_transport.client.compat.sable.ShipUtils;
 import me.mariz.antique_transport.client.compat.simulated.ShipDiagramRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
@@ -43,12 +45,11 @@ public class AntiqueTransportClient implements ClientModInitializer {
                         ShipDiagramRenderer.freeAll();
                         ShipCache.onDisconnect();
                     }));
-            net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents.LAST.register(context -> {
+            HudRenderCallback.EVENT.register((graphics, tickDeltaManager) -> {
+                if (!(Minecraft.getInstance().screen instanceof AtlasScreen)) return;
                 var level = Minecraft.getInstance().level;
                 if (level == null) return;
-
-                float pt = context.tickCounter().getGameTimeDeltaPartialTick(true);
-
+                float pt = tickDeltaManager.getGameTimeDeltaPartialTick(true);
                 for (SubLevelAccess ship : SableCompanion.INSTANCE.getAllIntersecting(level, ShipUtils.worldBounds(level))) {
                     if (ship instanceof ClientSubLevel sub) {
                         ShipDiagramRenderer.renderDiagram(sub.getUniqueId(), sub, pt);
